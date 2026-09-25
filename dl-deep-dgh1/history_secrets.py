ef6bb9d5db96743c374c52b385d2ee607bb89ac3
Naresh <naresh@das357.onmicrosoft.com>
2026-09-25 17:57:44 +0530

    Deep Scan regression dgh1: commit A (adds synthetic secrets)

---
+++ dl-deep-dgh1/history_secrets.py
@@ -0,0 +1,4 @@
+# temporary credentials -- remove before release
+AWS_ACCESS_KEY_ID = "AKIAOJIQLKPN4T8CWBCL"
+INTERNAL_SERVICE_TOKEN=d8802c7671ffd3ac80ba36f2739466f42670b62e
+# clone: git clone https://deploy:7kAopUlBWkKVvOY0lsbK@git.internal-corp.net/team/repo.git
